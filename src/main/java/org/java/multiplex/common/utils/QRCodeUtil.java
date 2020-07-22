d889237d5a948099964983018e8fd130fa4f4fc4
wp <wpbosser@163.com>
2020-07-22 18:19:23 +0800

    二维码工具类

--- src/main/java/org/java/multiplex/common/utils/QRCodeUtil.java
+++ src/main/java/org/java/multiplex/common/utils/QRCodeUtil.java
@@ -1,4 +1,4 @@
-package org.java.multiplex.util;
+package org.java.multiplex.common.utils;
 
 import com.google.zxing.*;
 import com.google.zxing.common.BitMatrix;
@@ -141,20 +141,6 @@ public class QRCodeUtil {
         return QRCodeUtil.decode(new File(path));
     }
 
-    public static void main(String[] args) throws Exception {
-        // 存放在二维码中的内容
-        String text = "www.baidu.com";
-        // 嵌入二维码的图片路径
-        String imgPath = "D:\\08188e84850f3eb92bf75b4ccf201b7.jpg";
-        // 生成的二维码的路径及名称
-        String destPath = "D:\\qrCode\\qrcode\\jam.jpg";
-        //生成二维码
-        QRCodeUtil.encode(text, imgPath, destPath, true);
-        // 解析二维码
-        String str = QRCodeUtil.decode(destPath);
-        // 打印出解析出的内容
-        System.out.println(str);
 
-    }
 
 }
